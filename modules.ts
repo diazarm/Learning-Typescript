46bec15987a6bea53eff690acc52927b60c54f73
diazarm <marceloardiaz@gmail.com>
2024-01-18 18:05:12 -0300

    learning

--- modules.ts
+++ modules.ts
@@ -10,3 +10,29 @@ module Saludos {
 
 Saludos.saludarHola()
 console.log(Saludos.numeroPi)
+
+//Aqui namespace (basicamente es lo mismo, pero hoy en dia se usa module)
+
+// Creas tu caja (namespace) llamada "Animalitos"
+namespace Animalitos {
+    // Dentro de la caja, pones a los gatos
+    export class Gato {
+        maullar() {
+            console.log("¡Miau!");
+        }
+    }
+
+    // También pones a los perros
+    export class Perro {
+        ladrar() {
+            console.log("¡Guau!");
+        }
+    }
+}
+
+// Ahora, puedes jugar con tus animalitos desde fuera del namespace
+const miGato = new Animalitos.Gato();
+miGato.maullar();
+
+const miPerro = new Animalitos.Perro();
+miPerro.ladrar();
